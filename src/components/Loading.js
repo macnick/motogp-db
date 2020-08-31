9c6415990139bae2e17a9bbc488c78b9b52460b2
macnick <nick@macexperts.gr>
2020-08-31 16:45:06 +0300

    Finished Loading loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import Header from './Header';
+// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+// import Header from './Header';
 
 const Loading = () => (
   <div className="loading-wrap">
-    <Header />
     <div className="loading">
-      <Loader type="ThreeDots" color="#f11" />
+      <Loader
+        type="ThreeDots"
+        color="#f11"
+        height={100}
+        width={100}
+        timeout={2000}
+      />
     </div>
   </div>
 );
